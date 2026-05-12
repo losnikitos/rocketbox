e9a52e32d27d8ecbeaf94ee3b621503b316c89bf
Nikita Popov <losnikitos@gmail.com>
2026-05-12 08:59:42 +0100

    Avo admin notion id

--- app/avo/resources/book.rb
+++ app/avo/resources/book.rb
@@ -4,7 +4,8 @@ class Avo::Resources::Book < Avo::BaseResource
   def fields
     field :id, as: :id
     field :title, as: :text
-    field :slug, as: :text, help: "URL segment, e.g. coffeeshop, tour-guide, barber"
+    field :slug, as: :text
+    field :notion_database_id, as: :text
     field :chapters, as: :has_many
     field :created_at, as: :date_time, readonly: true
     field :updated_at, as: :date_time, readonly: true
